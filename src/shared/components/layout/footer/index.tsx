7eec15b97a351e66cd297d77f948693b5e4ecf12
Anton Vasilenka <anton.vasilenko00@gmail.com>
2023-08-15 14:46:56 +0300

    style: improve footer reveal

--- src/shared/components/layout/footer/index.tsx
+++ src/shared/components/layout/footer/index.tsx
@@ -5,11 +5,12 @@ interface FooterProps extends HTMLProps<HTMLDivElement> {}
 const Footer: React.FC<FooterProps> = ({ ...props }) => {
 	return (
 		<footer
-			data-reveal
 			className='bg-white
 text-orange-500 flex justify-between items-center'>
 			<div className='section-container py-4 md:text-center'>
-				<span>&copy; {new Date().getFullYear()} Прораб Максим МСК</span>
+				<span data-reveal>
+					&copy; {new Date().getFullYear()} Прораб Максим МСК
+				</span>
 			</div>
 		</footer>
 	)

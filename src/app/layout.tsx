be77c8145e20feac4164d55968af7a29fa25eda4
Anton Vasilenka <anton.vasilenko00@gmail.com>
2023-08-22 12:44:56 +0300

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,19 +4,23 @@ import Header from '@/shared/components/layout/header'
 import { Open_Sans } from 'next/font/google'
 import Footer from '@/shared/components/layout/footer'
 import { ToastContainer } from 'react-toastify'
+import { Analytics } from '@vercel/analytics/react'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata = {
 	title: 'Ремонт Квартир | Москва',
-	description:
-		'Ремонт Квартир в Москве и Московской области от опытного прораба.',
+	description: 'Ремонт Квартир в Москве и Московской области.',
 	keywords: [
 		'ремонт',
+		'ремонт квартир',
 		'москва',
 		'московская область',
 		'ЖК',
 		'качественный ремонт',
+		'квартира',
+		'дом',
+		'смета',
 		'дизайн',
 		'интерьер',
 	],
@@ -35,6 +39,7 @@ export default function RootLayout({
 					<main className='flex-grow scroll-smooth'>{children}</main>
 					<Footer />
 				</div>
+				<Analytics />
 				<ToastContainer />
 			</body>
 		</html>
